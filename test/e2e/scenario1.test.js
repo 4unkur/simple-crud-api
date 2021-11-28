f1baaddfa85b82c9b8afbf41c89fb219faf271d9
Daiyrbek Artelov <dartelov@intelliants.com>
2021-11-29 00:06:29 +0600

    refactor: rename person test to scenario 1. add scenario 2

--- test/e2e/scenario1.test.js
+++ test/e2e/scenario1.test.js
@@ -3,13 +3,25 @@ const { makeFetch } = require('supertest-fetch');
 const uuid = require('uuid');
 const fetch = makeFetch(server);
 
-const MOCK_PERSON = { name: 'Ryan Dahl', age: 40, hobbies: ['js', 'nodejs', 'deno'] };
+const MOCK_PERSON = {
+  name: 'Ryan Dahl',
+  age: 40,
+  hobbies: ['js', 'nodejs', 'deno']
+};
 
-beforeEach(() => {
+const MOCK_UPDATED_PERSON = {
+  name: 'Edited name',
+  age: 100500,
+  hobbies: ['php', 'is', 'the', 'best'] // 😉
+};
+
+afterAll(() => {
   app.db.truncate();
 });
 
-describe('End-to-End Testing', () => {
+let person;
+
+describe('End-to-End Testing Scenario 1', () => {
   test('GET /person - should return array', async () => {
     await fetch('/person')
       .expect(200)
@@ -24,14 +36,12 @@ describe('End-to-End Testing', () => {
       headers: { 'Content-Type': 'application/json' }
     }).expect(201);
 
-    const body = await res.json();
-    expect(uuid.validate(body.id)).toBeTruthy();
-    delete body.id;
-    expect(body).toEqual(MOCK_PERSON);
+    person = await res.json();
+    expect(uuid.validate(person.id)).toBeTruthy();
+    expect(person).toEqual({ id: person.id, ...MOCK_PERSON });
   });
 
   test('GET /person/{personId} - should return person by personId', async () => {
-    const person = app.db.insert(MOCK_PERSON);
     const result = await fetch(`/person/${person.id}`)
       .expect(200)
       .expect('Content-Type', 'application/json')
@@ -41,13 +51,6 @@ describe('End-to-End Testing', () => {
   });
 
   test('PUT /person/{personId} - should update a person and return updated version', async () => {
-    const person = app.db.insert(MOCK_PERSON);
-    const MOCK_UPDATED_PERSON = {
-      name: 'Edited name',
-      age: 100500,
-      hobbies: ['php', 'is', 'the', 'best'] // 😉
-    };
-
     const res = await fetch(`/person/${person.id}`, {
       method: 'PUT',
       body: JSON.stringify(MOCK_UPDATED_PERSON),
@@ -60,8 +63,6 @@ describe('End-to-End Testing', () => {
   });
 
   test('DELETE /person/{personId} - should delete a person', async () => {
-    const person = app.db.insert(MOCK_PERSON);
-
     await fetch(`/person/${person.id}`, {
       method: 'DELETE'
     }).expect(204);
@@ -70,9 +71,6 @@ describe('End-to-End Testing', () => {
   });
 
   test('GET /person/{personId} - should return 404 if removed (non existing) person requested', async () => {
-    const person = app.db.insert(MOCK_PERSON);
-    app.db.delete(person.id);
-
     const res = await fetch(`/person/${person.id}`)
       .expect(404);
 
